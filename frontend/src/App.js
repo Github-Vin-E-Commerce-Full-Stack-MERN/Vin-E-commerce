50f7000a60fa6e8d7e39182200d5bc63b502ad32
lethang0922 <le.thang0922@gmail.com>
2024-07-15 16:34:53 -0700

    fix: change router structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './component/Header';
-
+import { Outlet } from 'react-router-dom';
+import Footer from './component/Footer';
 function App() {
   return (
-    <div>
-      <Header />
-    </div>
+    <>
+      <div><Header /></div><br />
+
+      <main className="content">
+        <Outlet />
+      </main>
+      <div><Footer /></div>
+    </>
   );
 }
 
--- frontend/src/component/Header.js
+++ frontend/src/component/Header.js
@@ -1,40 +1,45 @@
-import React from 'react'
-import logo from '../assest/logo.png'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import logo from '../assest/logo.png';
+import { Link } from 'react-router-dom';
 import { PiUserCircleFill } from "react-icons/pi";
 import { IoCartOutline } from "react-icons/io5";
+import { CiSearch } from 'react-icons/ci';
 
-function Header() {
+const Header = () => {
   return (
-    <header className='fixed shadow-md w-full h-16px px-2 md:px4 bg-[#0f172a]'>
-      {/* desktop */}
-      <div className='flex items center h-full justify-between '>
+    <header className='header shadow-md w-full h-16 px-2 md:px-4 bg-[#0f172a]'>
+      <div className='flex items-center h-full justify-between'>
         <Link to={""}>
-          <div className='h-20 '>
-            <img src={logo} className="h-full" />
+          <div className='h-20'>
+            <img src={logo} className="h-full" alt="Logo" />
           </div>
         </Link>
 
-        <div className='flex items-center text-white gap-5 md:gap-7'>
-          <nav className='flex gap-4 md:gap-7 text-base md:text-lg'>
-            <Link to={""}>Home</Link>
-            <Link to={"menu"}>Menu</Link>
-            <Link to={"about"}>About</Link>
-            <Link to={"contact"}>Contact</Link>
+        <div className='flex items-center text-white'>
+          <input
+            type='text'
+            placeholder='Enter Product'
+            className='p-2 rounded-l-full w-80 md:w-96'
+          />
+          <div className='text-2xl min-w-[60px] p-2 bg-white text-black rounded-r-full'>
+            <CiSearch />
+          </div>
+        </div>
 
-          </nav>
-          <div className='text-3xl text-slate-white'>
+        <div className='flex items-center text-white gap-5 md:gap-7'>
+          <div><Link to={""}>Home</Link></div>
+
+          <div className='text-3xl text-slate-white w-13'>
             <IoCartOutline />
           </div>
+
           <div className='text-3xl text-slate-white'>
             <PiUserCircleFill />
           </div>
         </div>
       </div>
-
-      {/* mobile */}
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
