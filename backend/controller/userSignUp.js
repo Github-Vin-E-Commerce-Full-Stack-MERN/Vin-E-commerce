41d5c7e5f81eac74ed75db8d42b03597baf3e489
Tu Ho <72232937+tuho9428@users.noreply.github.com>
2024-07-16 01:38:00 -0700

    update userSignUp route

--- backend/controller/userSignUp.js
+++ backend/controller/userSignUp.js
@@ -1,11 +1,57 @@
-async function userSignUpController(req,res){
-    try {
-        
-    } catch (error) {
-        res.json({
-            message: error,
-            error: true,
-            success: false,
-        })
+const userModel = require("../models/userModel");
+const bcrypt = require("bcryptjs");
+
+async function userSignUpController(req, res) {
+  try {
+    const { email, password, name } = req.body;
+    
+    const user = await userModel.findOne({email})
+
+    if (user) {
+      throw new Error("Already user exists");
     }
+
+    if (!email) {
+      throw new Error("Please provide email");
+    }
+
+    if (!password) {
+      throw new Error("Please provide password");
+    }
+
+    if (!name) {
+      throw new Error("Please provide name");
+    }
+
+    const salt = bcrypt.genSaltSync(12);
+    const hashPassword = bcrypt.hashSync(password, salt);
+
+    if(!hashPassword) {
+        throw new Error("Something is wrong");
+    }
+
+    const payload = {
+        ...req.body,
+        password: hashPassword
+    }
+
+    const userData = new userModel(payload);
+    const saveUser = userData.save()
+
+    res.status(201).json({
+        data: saveUser,
+        success: true,
+        error: false,
+        message: "User created successfully"
+    })
+
+  } catch (err) {
+    res.json({
+      message: err.message || err,
+      error: true,
+      success: false,
+    });
+  }
 }
+
+module.exports = userSignUpController
--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,11 @@ const router = require('./routes')
 
 const app = express()
 app.use(cors());
+app.use(express.json())
+
 app.use("/api",router)
 
-const PORT = 8000 || process.env.PORT;
+const PORT = 8080 || process.env.PORT;
 
 connectDB().then(() => {
   app.listen(PORT, () => {
