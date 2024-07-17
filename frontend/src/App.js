95a46120eda6b7e79d3814fcaca14798c2c534bd
lethang0922 <le.thang0922@gmail.com>
2024-07-17 00:57:30 -0700

    feat: fetching user detail

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,18 +4,39 @@ import { Outlet } from "react-router-dom";
 import Footer from "./component/Footer";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useEffect } from "react";
+import SummaryApi from "./common";
+import Context from "./context";
 
 function App() {
+
+  const fetchUserDetails = async () => {
+    const dataResponse = await fetch(SummaryApi.current_user.url, {
+      method: SummaryApi.current_user.method,
+      credentials: 'include'
+    })
+    const dataApi = await dataResponse.json()
+    console.log("data-user", dataResponse)
+  }
+
+  useEffect(() => {
+    // User details
+    fetchUserDetails()
+  }, [])
   return (
     <>
-      <ToastContainer />
+      <Context.Provider value={{
+        fetchUserDetails// user detail fetch
+      }}>
+        <ToastContainer />
 
-      <Header />
+        <Header />
 
-      <main className="min-h-[calc(100vh-120px)]">
-        <Outlet />
-      </main>
-      <Footer />
+        <main className="min-h-[calc(100vh-120px)]">
+          <Outlet />
+        </main>
+        <Footer />
+      </Context.Provider>
     </>
   );
 }
